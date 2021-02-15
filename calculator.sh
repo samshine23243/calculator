1febd62344bd714ecd7124c7a3093fba9494365c
samshine <sameershinde23243@gmail.com>
2021-02-15 11:29:35 +0530

    multiplication code added

--- calculator.sh
+++ calculator.sh
@@ -1,9 +1,4 @@
 read -p "Enter first number:" fno
 read -p "Enter second number:" sno
-if [ $fno -gt $sno ]
-then
-	diff=$(($fno-$sno))
-else
-	diff=$(($sno-$fno))
-fi
-echo "difference is:" $diff
+mul=$(($fno*$sno))
+echo "multiplication is:" $mul
